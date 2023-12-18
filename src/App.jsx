f36451fea169765ac9ea39bac1633322766b6546
GauravFrontend <gaurav9911093928@gmail.com>
2023-12-18 20:49:20 +0530

    reduxStore setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 
 function App() {
-
+  const dispatch = useDispatch()
+  const url = useSelector((state) => state.home.url)
   const apiTest = () => {
     fetchDataFromApi("/movie/popular")
       .then((res) => {
         console.log(res)
+        dispatch(getApiConfiguration(res))
       })
   }
 
@@ -18,7 +24,12 @@ function App() {
 
 
 
-  return <>app</>;
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
 }
 
 export default App;
